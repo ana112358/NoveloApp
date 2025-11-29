e7003f4a9f8293f36ba50a9c98791d4e030e13c1
Camila Braga <milabragalima@hotmail.com>
2025-11-29 16:54:17 -0300

    Alteração da paleta e de alguns estilos da lista de receitas salvas

--- lib/pages/lista_receitas_page.dart
+++ lib/pages/lista_receitas_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../state/receita_state.dart';
 import '../state/counter_app_state.dart';
+import '../models/receita_passo.dart';
 
 class ListaReceitasPage extends StatelessWidget {
   const ListaReceitasPage({super.key});
@@ -15,12 +16,21 @@ class ListaReceitasPage extends StatelessWidget {
       appBar: AppBar(title: const Text("Receitas Salvas")),
       body: receitaState.receitas.isEmpty
           ? Center(
-              child: Text(
-                "Nenhuma receita salva ainda.",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //estilo ícone nenhuma receita salva
+                  Icon(Icons.gesture, size: 80, color: colorScheme.tertiary),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Nenhuma receita salva',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: colorScheme.error),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
             )
           : ListView.builder(
@@ -28,6 +38,10 @@ class ListaReceitasPage extends StatelessWidget {
               itemCount: receitaState.receitas.length,
               itemBuilder: (_, i) {
                 final receita = receitaState.receitas[i];
+
+                //para alternar as cores dos cards:
+                final Color cardColor =
+                    (i % 2 == 0) ? colorScheme.secondary : colorScheme.surface;
 
                 // Calcula total de repetições e progresso
                 int totalRepeticoes = receita.passos
@@ -38,30 +52,47 @@ class ListaReceitasPage extends StatelessWidget {
 
                 return Card(
                   margin: const EdgeInsets.only(bottom: 12),
+
+                  //cor do card:
+                  color: cardColor,
+                  surfaceTintColor: Colors.transparent,
+
                   child: ListTile(
-                    title: Text(receita.titulo),
+                    title: Text(
+                      receita.titulo,
+                      style: TextStyle(
+                        color: colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           "${receita.passos.length} passos • Total: $totalRepeticoes repetições",
+                          style: TextStyle(color: colorScheme.primary),
                         ),
-                        const SizedBox(height: 6),
+                        const SizedBox(height: 8),
                         Row(
                           children: [
                             Expanded(
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(4),
                                 child: LinearProgressIndicator(
-                                  value: progresso,
-                                  minHeight: 6,
-                                ),
+                                    value: progresso,
+                                    minHeight: 6,
+                                    color: colorScheme.primary,
+                                    backgroundColor: colorScheme.onSecondary),
                               ),
                             ),
                             const SizedBox(width: 8),
                             Text(
                               "Passo ${receita.passoAtual + 1}/${receita.passos.length}",
-                              style: const TextStyle(fontSize: 12),
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: colorScheme.primary,
+                              ),
                             ),
                           ],
                         ),
@@ -72,14 +103,14 @@ class ListaReceitasPage extends StatelessWidget {
                               padding: const EdgeInsets.symmetric(
                                   horizontal: 8, vertical: 4),
                               decoration: BoxDecoration(
-                                color: colorScheme.tertiary,
-                                borderRadius: BorderRadius.circular(4),
+                                color: colorScheme.onSecondary,
+                                borderRadius: BorderRadius.circular(15),
                               ),
                               child: Text(
                                 "Concluída",
                                 style: TextStyle(
-                                  fontSize: 12,
-                                  color: colorScheme.tertiary,
+                                  fontSize: 14,
+                                  color: colorScheme.primary,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
@@ -96,8 +127,14 @@ class ListaReceitasPage extends StatelessWidget {
                               context, receitaState, receita.id!),
                         ),
                         IconButton(
-                          icon: Icon(Icons.play_arrow,
-                              color: colorScheme.tertiary),
+                          //estilo botão play:
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: colorScheme.tertiary,
+                          ),
+
+                          icon: Icon(Icons.play_arrow),
+
                           onPressed: () async {
                             if (receita.passos.isNotEmpty) {
                               // Carrega a receita mais recente do BD
